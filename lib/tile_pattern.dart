906870cc596d76bd0b740ebaa6b5c17c5c056674
Amr Eniou <amorenew@gmail.com>
2022-07-20 22:17:13 +0200

    cleanup

--- lib/tile_pattern.dart
+++ lib/tile_pattern.dart
@@ -1,4 +1,3 @@
-import 'package:cube/tile.dart';
 
 enum TilePattern {
   tile1(1, TileDirection.right),
@@ -9,46 +8,6 @@ enum TilePattern {
   final TileDirection direction;
 
   const TilePattern(this.value, this.direction);
-
-  TileDirection nextMove(TileDirection currentDirection) {
-    switch (currentDirection) {
-      case TileDirection.left:
-        return TileDirection.none;
-      case TileDirection.right:
-        if (this == TilePattern.tile1) {
-          return TileDirection.right;
-        }
-        return TileDirection.none;
-      case TileDirection.top:
-        return TileDirection.none;
-      case TileDirection.bottom:
-        return TileDirection.none;
-      default:
-        return TileDirection.none;
-    }
-  }
-
-  bool hasTurn() => value != 1;
-
-  TileDirection nextDirection(TileDirection startDirection, Tile tile) {
-    final level = tile.level;
-
-    var tileDirection = TileDirection.none;
-
-    if (startDirection == TileDirection.left) {
-      if (tile.left == tile.right) {
-        tileDirection = TileDirection.right;
-      }
-
-      if (tile.left == tile.top && level > 1) {
-        tileDirection = TileDirection.top;
-      }
-      if (tile.left == tile.bottom && level < 3) {
-        tileDirection = TileDirection.bottom;
-      }
-    }
-    return tileDirection;
-  }
 }
 
 enum TileDirection {
